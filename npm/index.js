d4ec47e87e577b43c606313ae197d894f244af55
Greg Walker <mgwalker@gmail.com>
2017-03-24 18:54:39 -0500

    If adding docker, don't actually install npm modules locally

--- npm/index.js
+++ npm/index.js
@@ -4,11 +4,17 @@ const promisedExec = require('../promisedExec');
 const install = require('./install');
 const scripts = require('./scripts');
 
+const dockerPlugin = require('../plugins/docker');
+
 function init(pieces) {
   return promisedExec('npm init --yes').then(() => pieces);
 }
 
 function build(pieces) {
+  if (pieces.includes(dockerPlugin)) {
+    log.info(`${chalk.cyan('npm run build')} skipping build because that should happen in docker`);
+    return Promise.resolve();
+  }
   log.info(`${chalk.cyan('npm run build')} running initial build`);
   return promisedExec('npm run build').then(() => pieces);
 }
--- npm/install.js
+++ npm/install.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+const request = require('request-promise');
 const chalk = require('chalk');
 const log = require('../log');
 const promisedExec = require('../promisedExec');
+
+const dockerPlugin = require('../plugins/docker');
 
 function getNPMDepList(pieces, which) {
   const dependencies = pieces.reduce((set, piece) => {
@@ -10,9 +14,36 @@ function getNPMDepList(pieces, which) {
   return [...dependencies];
 }
 
+function addDepsToPackageJSON(modules, saveExt = false) {
+  log.info(`${chalk.cyan('npm install')} adding ${modules.length} ${saveExt ? `${saveExt} ` : ''}dependencies`);
+  const promises = [];
+
+  for (const module of modules) {
+    log.info(`${chalk.cyan('npm install')} --> ${module}`);
+    promises.push(request.get(`https://registry.npmjs.org/${module}/latest`, { json: true }));
+  }
+
+  return Promise.all(promises).then((args) => {
+    const dependencies = args.sort((a, b) => a.name > b.name).map(arg => ({ [arg.name]: arg.version }));
+    const propertyName = `${saveExt || ''}${saveExt ? 'D' : 'd'}ependencies`;
+    const json = Object.assign(...dependencies);
+
+    const packageJSON = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }));
+    packageJSON[propertyName] = json;
+    fs.writeFileSync('package.json', JSON.stringify(packageJSON, false, 2), { encoding: 'utf-8' });
+  });
+}
+
 function npmInstall(modules, saveExt = false) {
   log.info(`${chalk.cyan('npm install')} installing ${modules.length} ${saveExt ? `${saveExt} ` : ''}dependencies`);
   return promisedExec(`npm install ${modules.join(' ')} --save${saveExt ? `-${saveExt}` : ''}`);
+}
+
+function getInstallFn(pieces) {
+  if (pieces.includes(dockerPlugin)) {
+    return addDepsToPackageJSON;
+  }
+  return npmInstall;
 }
 
 function filterHasNpm(pieces, deps = 'deps') {
@@ -21,13 +52,13 @@ function filterHasNpm(pieces, deps = 'deps') {
 
 function install(pieces) {
   const pieceModules = getNPMDepList(filterHasNpm(pieces), 'deps');
-  return npmInstall(pieceModules).then(() => pieces);
+  return getInstallFn(pieces)(pieceModules).then(() => pieces);
 }
 
 function installDev(pieces) {
   const defaultDevDependences = ['eslint', 'eslint-config-airbnb-base', 'eslint-plugin-import'];
   const pieceModules = getNPMDepList(filterHasNpm(pieces, 'devDeps'), 'devDeps');
-  return npmInstall([...defaultDevDependences, ...pieceModules], 'dev').then(() => pieces);
+  return getInstallFn(pieces)([...defaultDevDependences, ...pieceModules], 'dev').then(() => pieces);
 }
 
 module.exports = {
